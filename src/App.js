104f303ee99b5d8c335f65cbeaac8c2796821d9a
PinjaMultala <multalapinja@yahoo.com>
2019-02-06 13:25:49 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import Title from "./Components/Title";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
+
+
+const API_KEY = "c67e5bdd523adc49ff8810e2278fe0ea";
+
+class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value; //tämä asettaa formiin kirjoitetut kaumungin ja maan api-osoitteeseen jotta osataan hakea oikean paikan tiedot.
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const data = await api_call.json(); //suodattaa datan jsoniksi
+    if (city && country) {
+      console.log("MISSÄ OLEN", data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                
+                  <Title />
+                  
+                </div>
+                <div className="col-xs-7 form-cintainer">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Title.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Title extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className="title-conteiner__title">Weather Finder</h2>
+                
+            </div>
+        );
+    }
+}
+
+
+export default Title;
